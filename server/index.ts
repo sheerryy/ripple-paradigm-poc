8ea0d5f2396de5d30c05b77acb3e2023efb9e6bd
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-08-31 21:25:41 +0500

    add an Http Server

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,14 @@
-const message: string = 'Hello World!';
+import * as express from 'express';
 
-console.log(message);
+const app: express.Application = express();
+const http = require("http").Server(app);
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+const server = http.listen(PORT, function () {
+    console.log(`listening on *:${PORT}`);
+});
