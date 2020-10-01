fed516e985fb7824d4d2d6bd3fc072fb4185bd25
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-01 22:30:44 +0500

    add routes

---
+++ server/src/routes/index.ts
@@ -0,0 +1,5 @@
+import ReporstRoute from './reports/reports.route';
+
+export {
+    ReporstRoute
+}
---
+++ server/src/routes/reports/reports.route.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+
+import { ReportsController } from "@modules/reports/controllers/reports.controller";
+
+const router = Router();
+const controller = new ReportsController();
+
+router.get('/:id', controller.getReport);
+
+router.get('/', controller.getReports);
+
+router.post('/', controller.createReport);
+
+router.put('/:id', controller.updateReport);
+
+router.delete('/:id', controller.deleteReport);
+
+export default router;
