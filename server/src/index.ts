2cd7c7e2f3fdfe6debf600ee76ca827b08454714
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-02 23:02:37 +0500

    add SocketIoMiddleware in app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import { Server } from 'http';
 import express from 'express';
+import 'module-alias/register';
+
+import { SocketIoMiddleware } from '@middlewares'
 
 import { ReporstRoute } from "./routes";
 
 const app: express.Application = express();
 const http = new Server(app);
-
 const PORT = process.env.PORT || 3001;
+
+app.use(new SocketIoMiddleware(http).responseEmitter);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
@@ -15,5 +19,5 @@ app.get('/', (req, res) => {
 app.use('/reports', ReporstRoute);
 
 const server = http.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+    console.log(`App started on http://localhost:${PORT}`);
 });
