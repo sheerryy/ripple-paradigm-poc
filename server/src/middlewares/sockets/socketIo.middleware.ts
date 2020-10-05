79329b631b4f94a049f51cd7c456ffe6e5840ec0
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-05 20:42:05 +0500

    replace end event with res.json event

--- server/src/middlewares/sockets/socketIo.middleware.ts
+++ server/src/middlewares/sockets/socketIo.middleware.ts
@@ -10,13 +10,28 @@ class SocketIoMiddleware {
   }
 
   responseEmitter(req: Request, res: Response, next: NextFunction) {
-    req.on('end', (data) => {
-      if (['GET', 'POST', 'PATCH', 'DELETE'].includes(req.method)) {
-        console.log(res)
-      }
-    });
+    // req.on('end', (data: any) => {
+    //   if (['GET', 'POST', 'PATCH', 'DELETE'].includes(req.method)) {
+    //     res.json = (data2: any) => {
+    //       console.log(data2); return res.json.call(res, data2);
+    //     }
+    //   }
+    // });
 
-    next();
+    // next()
+    try {
+      if (['GET', 'POST', 'PATCH', 'DELETE'].includes(req.method)) {
+        const oldJSON = res.json;
+        res.json = (data) => {
+          res.json = oldJSON;
+          console.log(data);
+          return oldJSON.call(res, data);
+        }
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
