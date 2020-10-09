d52dfbb90d0326f9308cd32df2d024ff93cf264f
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-09 20:53:42 +0500

    add socket implementation in responseEmitter

--- server/src/middlewares/sockets/socketIo.middleware.ts
+++ server/src/middlewares/sockets/socketIo.middleware.ts
@@ -4,15 +4,39 @@ import {NextFunction, Request, Response} from "express";
 
 class SocketIoMiddleware {
   socketServer: SocketIO.Server;
+  socket: SocketIO.Socket;
 
   constructor(httpServer: Server) {
-    this.socketServer = SocketIO(httpServer)
+    this.socketServer = SocketIO(httpServer);
+    this.socketServer.on('connection', (socket: SocketIO.Socket) => {
+      console.log(`user (id: ${socket.id}) connected`);
+      this.socket = socket;
+    });
+    this.socketServer.on("disconnect", () => {
+      console.log(`user (id: ${this.socket.id} disconnected`);
+      this.socket = null;
+    });
   }
 
   responseEmitter = (context: string) => (req: Request, res: Response, next: NextFunction) => {
     res.on("finish", () => {
-      if (['POST', 'PATCH', 'DELETE'].indexOf(req.method) !== -1) {
+      if (['POST', 'PUT', 'PATCH', 'DELETE'].indexOf(req.method) !== -1) {
         console.log(`${req.method} request on ${context}`);
+ // @START: check status code
+        switch (req.method) {
+          case 'POST':
+            this.socket.emit(`emitter/${context}`, req.method);
+            break;
+          case 'PUT':
+            this.socket.emit(`emitter/${context}`, `${req.method}_${req.params.id}`);
+            break;
+          case 'PATCH':
+            this.socket.emit(`emitter/${context}`, `${req.method}_${req.params.id}`);
+            break;
+          case 'DELETE':
+            this.socket.emit(`emitter/${context}`, `${req.method}_${req.params.id}`);
+            break;
+        }
       }
     });
 
