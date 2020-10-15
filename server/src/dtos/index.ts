42d231ae296904d6b190208675b6f51e8c0a1cda
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-15 23:14:21 +0500

    add AuthorsResponse dto

---
+++ server/src/dtos/index.ts
@@ -0,0 +1,6 @@
+// Response DTOs
+export { AuthorsResponse } from './authors/authors.response.dto';
+export { ReportsResponse } from './reports/reports.response.dto';
+
+// Mappers
+export { reportsToReportResponse } from './reports/reports-reponse.mapper';
