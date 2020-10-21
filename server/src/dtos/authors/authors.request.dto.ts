b65f8e4a7606d9e4341be0bba9f2cf77546951b7
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-21 21:01:36 +0500

    add AuthorRequest DTO

---
+++ server/src/dtos/authors/authors.request.dto.ts
@@ -0,0 +1,3 @@
+export type AuthorsRequest = {
+  name: string;
+};
--- server/src/dtos/index.ts
+++ server/src/dtos/index.ts
@@ -2,6 +2,9 @@
 export { AuthorsResponse } from './authors/authors.response.dto';
 export { ReportsResponse } from './reports/reports.response.dto';
 
+// Request DTOs
+export { AuthorsRequest } from './authors/authors.request.dto';
+
 // Mappers
 export { authorsToAuthorResponse } from './authors/authors-reponse.mapper';
 export { reportsToReportResponse } from './reports/reports-reponse.mapper';
