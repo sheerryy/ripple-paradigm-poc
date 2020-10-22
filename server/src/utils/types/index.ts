1a018bd8b8be512ccf7a67d0764aa6614a8dc62d
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-22 23:17:42 +0500

    remove unused DeepPartial type

--- server/src/utils/types/index.ts
+++ server/src/utils/types/index.ts
@@ -1,5 +1,4 @@
 export {
-  DeepPartial,
   ErrorResponse,
   EmitterResponse,
   DependentContext,
--- server/src/utils/types/types.utils.ts
+++ server/src/utils/types/types.utils.ts
@@ -1,7 +1,3 @@
-export type DeepPartial<T> = {
-    [P in keyof T]?: DeepPartial<T[P]>;
-};
-
 export type EmitterRequestMethod = 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
 export type ErrorResponse = {
