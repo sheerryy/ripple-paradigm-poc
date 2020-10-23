d3bc5df2b1a1c8dce9900635433e68b9d4f18aea
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-24 04:13:02 +0500

    add mongoObjectId in utils

--- server/src/utils/types/index.ts
+++ server/src/utils/types/index.ts
@@ -1,6 +1,10 @@
+// Type
 export {
   ErrorResponse,
   EmitterResponse,
   DependentContext,
   EmitterRequestMethod,
 } from './types.utils';
+
+// Common Functions
+export { mongoObjectId } from './object-id-generator.utils';
---
+++ server/src/utils/types/object-id-generator.utils.ts
@@ -0,0 +1,10 @@
+// Copied from https://gist.github.com/solenoid/1372386
+
+export const mongoObjectId = () => {
+  // tslint:disable-next-line:no-bitwise
+  const timestamp = (new Date().getTime() / 1000 | 0).toString(16);
+  return timestamp + 'xxxxxxxxxxxxxxxx'.replace(/[x]/g, () => {
+    // tslint:disable-next-line:no-bitwise
+    return (Math.random() * 16 | 0).toString(16);
+  }).toLowerCase();
+};
