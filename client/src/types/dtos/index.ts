852f2f2134fd0d881c1ee5643b44111241ccf338
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-10-28 22:28:06 +0500

    add dtos in types

---
+++ client/src/types/dtos/index.ts
@@ -0,0 +1,7 @@
+// Response DTOs
+export type AuthorsResponse = import('./authors/authors.response.dto').AuthorsResponse;
+export type ReportsResponse = import('./reports/reports.response.dto').ReportsResponse;
+
+// Request DTOs
+export type AuthorsRequest = import('./authors/authors.request.dto').AuthorsRequest;
+export type ReportsRequest = import('./reports/reports.response.dto').ReportsResponse;
