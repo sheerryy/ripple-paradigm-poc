74ca54864d942bfc952340572d2bd131626afa64
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-07 17:14:15 +0500

    update axios utils

--- client/src/utils/axios/axios.utils.ts
+++ client/src/utils/axios/axios.utils.ts
@@ -1,14 +1,27 @@
 import axios from 'axios';
 import * as qs from 'querystring';
+import {ErrorResponse} from "../../types/responses";
 
-export const doGet = async (path: string, params = {}) => {
-  return axios({
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    url: `${path}?${qs.stringify(params)}`,
-  });
+export const doGet = async (path: string, params = {}): Promise<any | ErrorResponse> => {
+  try {
+    const response = await axios({
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      url: `${path}?${qs.stringify(params)}`,
+    });
+
+    if (String(response.status).charAt(0) === '2') {
+      return response?.data?.data;
+    }
+
+    return response?.data as ErrorResponse;
+  } catch (err) {
+    console.log(err);
+
+    return err?.response?.data as ErrorResponse;
+  }
 };
 
 export const doPost = async (path: string, body = {}, params = {}) => {
