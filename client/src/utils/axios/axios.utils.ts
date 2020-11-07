8de2e62b2e5736fed2599de9b8d9c5831de0b1b1
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-07 17:23:35 +0500

    update doPost, doPut, and doDelete method in axios utils

--- client/src/utils/axios/axios.utils.ts
+++ client/src/utils/axios/axios.utils.ts
@@ -12,7 +12,7 @@ export const doGet = async (path: string, params = {}): Promise<any | ErrorRespo
       url: `${path}?${qs.stringify(params)}`,
     });
 
-    if (String(response.status).charAt(0) === '2') {
+    if (response.status === 200) {
       return response?.data?.data;
     }
 
@@ -25,34 +25,70 @@ export const doGet = async (path: string, params = {}): Promise<any | ErrorRespo
 };
 
 export const doPost = async (path: string, body = {}, params = {}) => {
-  return axios({
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    url: `${path}?${qs.stringify(params)}`,
-    data: body,
-  });
+  try {
+    const response = await axios({
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      url: `${path}?${qs.stringify(params)}`,
+      data: body,
+    });
+
+    if (String(response.status).charAt(0) === '2') {
+      return response?.data?.data;
+    }
+
+    return response?.data as ErrorResponse;
+  } catch (err) {
+    console.log(err);
+
+    return err?.response?.data as ErrorResponse;
+  }
 };
 
 export const doPut = async (path: string, body = {}, params = {}) => {
-  return axios({
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    url: `${path}?${qs.stringify(params)}`,
-    data: body,
-  });
+  try {
+    const response = await axios({
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      url: `${path}?${qs.stringify(params)}`,
+      data: body,
+    });
+
+    if (String(response.status).charAt(0) === '2') {
+      return response?.data?.data;
+    }
+
+    return response?.data as ErrorResponse;
+  } catch (err) {
+    console.log(err);
+
+    return err?.response?.data as ErrorResponse;
+  }
 };
 
 export const doDelete = async (path: string, params = {}) => {
-  return axios({
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-    },
-    url: `${path}?${qs.stringify(params)}`,
-  });
+  try {
+    const response = await axios({
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      url: `${path}?${qs.stringify(params)}`,
+    });
+
+    if (String(response.status).charAt(0) === '2') {
+      return response?.data?.data;
+    }
+
+    return response?.data as ErrorResponse;
+  } catch (err) {
+    console.log(err);
+
+    return err?.response?.data as ErrorResponse;
+  }
 };
 
