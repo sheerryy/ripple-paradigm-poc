d8e2d26ef0b84f389a5f525360af0df54e681cca
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-16 23:46:40 +0500

    add tableRoot and tableTitle styles for Basic Table

--- client/src/components/tables/BasicTable.style.tsx
+++ client/src/components/tables/BasicTable.style.tsx
@@ -1,6 +1,12 @@
 import { createStyles, Theme } from "@material-ui/core";
 
-export const BasicTableStyle = (theme: Theme) => createStyles({
+export const BasicTableStyle = ({ spacing }: Theme) => createStyles({
+  tableRoot: {
+    flexGrow: 1,
+  },
+  tableTitle: {
+    margin: `${spacing(1.2)}px 0px`
+  },
   table: {
     minWidth: 650,
   },
