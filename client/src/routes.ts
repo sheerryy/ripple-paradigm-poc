bf259e3a85b50cbb33560604af79fc93f4e5faac
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-20 19:47:40 +0500

    add Author.View.tsx in routes.ts

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,6 +1,9 @@
 import React from "react";
 
-import { HomeView } from "./views";
+import {
+  HomeView,
+  AuthorView,
+} from "./views";
 
 
 export const routes: Routes[] = [
@@ -8,11 +11,15 @@ export const routes: Routes[] = [
     path: '/*',
     exact: false,
     component: HomeView,
+  },
+  {
+    path: '/author',
+    component: AuthorView,
   }
 ]
 
 export type Routes = {
   path: string,
-  exact: boolean,
+  exact?: boolean,
   component: React.FunctionComponent,
 }
