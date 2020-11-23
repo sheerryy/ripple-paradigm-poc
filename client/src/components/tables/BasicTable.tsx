d51be6ef1ec8f7b306886efda41f4e0afbee20e2
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-23 20:03:16 +0500

    Update BasicTable in components

--- client/src/components/tables/BasicTable.tsx
+++ client/src/components/tables/BasicTable.tsx
@@ -13,28 +13,17 @@ import {
 
 import { BasicTableStyle } from './BasicTable.style';
 
-
 interface PropTypes {
   classes: {
     tableRoot: string,
     table: string,
     tableTitle: string,
-  }
+  },
+  tableHeadings: string[],
+  tableData: string[][]
 }
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function BasicTable({ classes }: PropTypes) {
+function BasicTable({ classes, tableHeadings = [], tableData }: PropTypes) {
   return (
     <div className={classes.tableRoot}>
       <Typography className={classes.tableTitle} variant="h5" align="left">
@@ -44,23 +33,13 @@ function BasicTable({ classes }: PropTypes) {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            {tableHeadings.map((heading, headinIndex) => <TableCell key={headinIndex}>{heading}</TableCell>)}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+          {tableData.map((row, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {row.map((cell, cellIndex) => <TableCell key={cellIndex}>{row}</TableCell>)}
             </TableRow>
           ))}
         </TableBody>
