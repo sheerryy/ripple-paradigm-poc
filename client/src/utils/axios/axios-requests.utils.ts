6828f4709ef34d41bb65b0601f401a379d444db0
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-24 23:56:35 +0500

    remove unused dependencies from axios-requests.utils.ts

--- client/src/utils/axios/axios-requests.utils.ts
+++ client/src/utils/axios/axios-requests.utils.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
 import * as qs from 'querystring';
-import { AxiosUtilResponse, ErrorResponse } from "../../types/responses";
+import { AxiosUtilResponse } from "../../types/responses";
 import {doJsonRequest} from "./axios.utils";
 
 export const doGet = async (path: string, params = {}): Promise<AxiosUtilResponse> => {
