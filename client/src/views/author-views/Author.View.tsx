661b3cfddb3fc9bb1effbfb04fc352f3ffe4c84c
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-24 20:38:29 +0500

    add AuthorTable.tsx in Author.View.tsx

--- client/src/views/author-views/Author.View.tsx
+++ client/src/views/author-views/Author.View.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {Grid, Paper} from "@material-ui/core";
-import {BasicTable} from "../../components";
+import AuthorTable from "./components/AuthorTable";
 
 function AuthorView() {
   return (
     <Grid container justify="center">
       <Grid item sm={12} md={10}>
         <Paper style={{padding: '2em', height: '100%'}}>
-          <BasicTable></BasicTable>
+          <AuthorTable></AuthorTable>
         </Paper>
       </Grid>
     </Grid>
