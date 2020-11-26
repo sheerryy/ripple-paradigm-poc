5f70c93a5d356e011f1550fcd78399e6c059746b
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-26 23:31:05 +0500

    implement authors api in AuthorTable.tsx

--- client/src/views/author-views/components/AuthorTable.tsx
+++ client/src/views/author-views/components/AuthorTable.tsx
@@ -1,5 +1,38 @@
-function AuthorTable() {
-  return (<>/<>)
+import React, { useState, useEffect } from 'react';
+import { Skeleton } from "@material-ui/lab";
+
+import { getAuthors } from "../../../apis/authors/authors.api";
+import {BasicTable} from "../../../components";
+
+function AuthorTable(){
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result: any = await getAuthors();
+
+      console.log(`result`, result)
+      if (result.errorCode) {
+        console.log(result.message)
+      } else {
+        debugger
+        setAuthors(result);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    console.log(authors);
+  }, [authors])
+
+  return authors?.length ?
+    <BasicTable
+      tableHeadings={Object.keys(authors[0])}
+      tableData={authors.map((author) => Object.values(author))}
+    /> :
+    <Skeleton variant="rect" height={400} />;
 }
 
 export default AuthorTable;
