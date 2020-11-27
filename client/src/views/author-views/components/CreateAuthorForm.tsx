4877af120bdebfee20cb8c4504f256510c31e724
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-11-27 23:13:40 +0500

    add CreateAuthorForm.tsx

---
+++ client/src/views/author-views/components/CreateAuthorForm.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Skeleton } from "@material-ui/lab";
+
+import { getAuthors } from "../../../apis/authors/authors.api";
+import {BasicTable} from "../../../components";
+import {AuthorsRequest} from "../../../types/dtos";
+
+function CreateAuthorForm(){
+
+
+  return <></>;
+}
+
+export default CreateAuthorForm;
