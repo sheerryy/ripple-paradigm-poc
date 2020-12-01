5f9d27886cef86357ba3dcccaa7df4092ffb50d5
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-01 21:54:34 +0500

    Revert "a"
    This reverts commit acc8d2c7

--- client/src/utils/common/common.utils.ts
+++
@@ -1,4 +0,0 @@
-export const snakeCaseToNormal = (snakeCase: string): string => {
-  const result: string = snakeCase.replace(/([A-Z])/g, " $1");
-  return result.charAt(0).toUpperCase() + result.slice(1);
-};
