c74bb53bc59d3f00737d82e7b5fd483ac2554167
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-07 20:40:04 +0500

    implement function to add new Author in AuthorTable.tsx

--- client/src/views/author-views/components/AuthorTable.tsx
+++ client/src/views/author-views/components/AuthorTable.tsx
@@ -1,33 +1,95 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 
 import { getAuthors } from "../../../apis/authors/authors.api";
 import {BasicTable} from "../../../components";
+import {camelCaseToNormal} from "../../../utils";
+import {AuthorsResponse} from "../../../types/dtos";
+import SaveAuthorForm from "./SaveAuthorForm";
 
 function AuthorTable(){
-  const [authors, setAuthors] = useState([])
+  const [authors, setAuthors] = useState<AuthorsResponse[]>([]);
+  const [selectedAuthor, setSelectedAuthor] = useState<AuthorsResponse | undefined>(undefined);
+  const [saveAuthorModal, setSaveAuthorModal] = useState<{ open: boolean, title: string }>({
+    open: false,
+    title: 'Add new Author',
+  });
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result: any = await getAuthors();
-
-      console.log(`result`, result)
-      if (result.errorCode) {
-        console.log(result.message)
-      } else {
-        setAuthors(result);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    const result: any = await getAuthors();
+
+    console.log(`result`, result)
+    if (result.errorCode) {
+      console.log(result.message)
+    } else {
+      setAuthors(result);
+    }
+  };
+
+
+  const handleCreateAction = () => {
+    setSaveAuthorModal({
+      open: true,
+      title: 'Add new Author.'
+    });
+  }
+
+  const handleEditAction = (id: string) => {
+    console.log(`edit: ${id}`);
+    // setSelectedAuthor(authors.find((author) => author.id === id));
+    // setEditAuthorModal(true);
+  }
+
+  const handleDeleteAction = (id: string) => {
+    console.log(`delete: ${id}`);
+    // setSelectedAuthor(authors.find((author) => author.id === id));
+    // setEditAuthorModal(true);
+  }
+
+  const handleAuthorDialogClose = () => {
+    setSaveAuthorModal((obj) => ({ ...obj, open: false }));
+    fetchData();
+  }
+
   return authors?.length ?
-    <BasicTable
-      title="Authors"
-      tableHeadings={Object.keys(authors[0])}
-      tableData={authors.map((author) => Object.values(author))}
-    /> :
+    <div>
+      <BasicTable
+        title="Authors"
+        actions={{
+          action: true,
+          allowCreate: true,
+          handleCreate: handleCreateAction,
+          editAction: true,
+          handleEdit: handleEditAction,
+          deleteAction: true,
+          handleDelete: handleDeleteAction,
+        }}
+        tableHeadings={Object.keys(authors[0]).map((heading) => camelCaseToNormal(heading))}
+        tableData={authors.map((author) => Object.values(author))}
+        tableDataIds={authors.map((author) => author.id)}
+      />
+      <Dialog
+        open={saveAuthorModal.open}
+        onClose={handleAuthorDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{saveAuthorModal.title}</DialogTitle>
+        <DialogContent>
+          <SaveAuthorForm handleClose={handleAuthorDialogClose}/>
+        </DialogContent>
+      </Dialog>
+    </div>
+    :
     <Skeleton variant="rect" height={400} />;
 }
 
