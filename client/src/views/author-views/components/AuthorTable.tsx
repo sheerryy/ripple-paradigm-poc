d64fc5968aaa6aade6ca7bf62f084d55869de7f4
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-11 23:27:19 +0500

    complete the flow to delete author

--- client/src/views/author-views/components/AuthorTable.tsx
+++ client/src/views/author-views/components/AuthorTable.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import {
+  Button,
   Dialog,
-  DialogContent,
   DialogTitle,
+  DialogActions,
+  DialogContent, Snackbar,
 } from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
+import {Alert, Color, Skeleton} from "@material-ui/lab";
 
-import { getAuthors } from "../../../apis/authors/authors.api";
-import {BasicTable} from "../../../components";
-import {camelCaseToNormal} from "../../../utils";
-import {AuthorsResponse} from "../../../types/dtos";
+import { deleteAuthor, getAuthors } from "../../../apis/authors/authors.api";
+import { BasicTable } from "../../../components";
+import { camelCaseToNormal } from "../../../utils";
+import { AuthorsResponse } from "../../../types/dtos";
 import SaveAuthorForm from "./SaveAuthorForm";
 
 function AuthorTable(){
@@ -18,6 +20,12 @@ function AuthorTable(){
   const [saveAuthorModal, setSaveAuthorModal] = useState<{ open: boolean, title: string }>({
     open: false,
     title: 'Add new Author',
+  });
+  const [deleteAuthorModal, setDeleteAuthorModal] = useState<boolean>(false);
+  const [snackBar, setSnackBar] = useState<{ open: boolean, message: string, severity: Color }>({
+    open: false,
+    message: '',
+    severity: 'error'
   });
 
   useEffect(() => {
@@ -41,6 +49,7 @@ function AuthorTable(){
 
 
   const handleCreateAction = () => {
+    setSelectedAuthor(undefined);
     setSaveAuthorModal({
       open: true,
       title: 'Add new Author.'
@@ -58,12 +67,39 @@ function AuthorTable(){
 
   const handleDeleteAction = (id: string) => {
     console.log(`delete: ${id}`);
-    // setSelectedAuthor(authors.find((author) => author.id === id));
-    // setEditAuthorModal(true);
+    setSelectedAuthor(authors.find((author) => author.id === id));
+    setDeleteAuthorModal(true);
+  }
+
+  const handleDeleteAuthor = async (id?: string) => {
+    if (id) {
+      const response: any = await deleteAuthor(id);
+
+      if (response.error) {
+        setSnackBar({
+          open: true,
+          severity: 'error',
+          message: 'Error deleting Author.'
+        });
+      } else {
+        handleDeleteAuthorDialogClose();
+      }
+    } else {
+      setSnackBar({
+        open: true,
+        severity: 'error',
+        message: 'Id not found.'
+      })
+    }
   }
 
   const handleAuthorDialogClose = () => {
     setSaveAuthorModal((obj) => ({ ...obj, open: false }));
+    fetchData();
+  }
+
+  const handleDeleteAuthorDialogClose = () => {
+    setDeleteAuthorModal(false);
     fetchData();
   }
 
@@ -87,14 +123,41 @@ function AuthorTable(){
       <Dialog
         open={saveAuthorModal.open}
         onClose={handleAuthorDialogClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+        aria-labelledby="save-author"
+        aria-describedby="dialog to save author"
       >
         <DialogTitle id="alert-dialog-title">{saveAuthorModal.title}</DialogTitle>
         <DialogContent>
           <SaveAuthorForm author={selectedAuthor} handleClose={handleAuthorDialogClose}/>
         </DialogContent>
       </Dialog>
+      <Dialog
+        open={deleteAuthorModal}
+        onClose={handleDeleteAuthorDialogClose}
+        aria-labelledby="delete-author"
+        aria-describedby="dialog to delete author"
+      >
+        <DialogTitle id="alert-dialog-title">Delete Author: {selectedAuthor?.id}</DialogTitle>
+        <DialogContent>
+          Are you sure that you want to delete Author <b>{selectedAuthor?.name}</b>?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteAuthorDialogClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={() => handleDeleteAuthor(selectedAuthor?.id)} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={snackBar.open}
+        autoHideDuration={5000}
+        onClose={() => setSnackBar((obj) => ({ ...obj, open: false }))}>
+        <Alert severity={snackBar.severity}>
+          {snackBar.message}
+        </Alert>
+      </Snackbar>
     </div>
     :
     <Skeleton variant="rect" height={400} />;
