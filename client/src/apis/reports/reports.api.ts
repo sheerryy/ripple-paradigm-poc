2234ee86517bf329f8dcd35911477de29245a43e
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-12 19:42:00 +0500

    export all report api methods

--- client/src/apis/reports/reports.api.ts
+++ client/src/apis/reports/reports.api.ts
@@ -6,7 +6,7 @@ import { ReportsRequest, ReportsResponse } from "../../types/dtos";
 
 const config = getConfig();
 
-const getReports = async (): Promise<ReportsResponse[] | ErrorResponse> => {
+export const getReports = async (): Promise<ReportsResponse[] | ErrorResponse> => {
   let reports: ReportsResponse[];
 
   const response = await doGet(`${config.API_BASE_URL}reports/`);
@@ -20,7 +20,7 @@ const getReports = async (): Promise<ReportsResponse[] | ErrorResponse> => {
   return reports;
 };
 
-const getReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
+export const getReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
   let report: ReportsResponse;
 
   const response = await doGet(`${config.API_BASE_URL}reports/${id}`);
@@ -34,7 +34,7 @@ const getReport = async (id: string): Promise<ReportsResponse | ErrorResponse> =
   return report;
 };
 
-const createReport = async (reportDto: ReportsRequest): Promise<ReportsResponse| ErrorResponse> => {
+export const createReport = async (reportDto: ReportsRequest): Promise<ReportsResponse| ErrorResponse> => {
   let report: ReportsResponse;
 
   const response = await doPost(`${config.API_BASE_URL}reports/`, reportDto);
@@ -48,7 +48,7 @@ const createReport = async (reportDto: ReportsRequest): Promise<ReportsResponse|
   return report;
 };
 
-const updateReport = async (id: string, reportDto: Partial<ReportsRequest>): Promise<ReportsResponse | ErrorResponse> => {
+export const updateReport = async (id: string, reportDto: Partial<ReportsRequest>): Promise<ReportsResponse | ErrorResponse> => {
   let report: ReportsResponse;
 
   const response = await doPut(`${config.API_BASE_URL}reports/${id}`, reportDto);
@@ -62,7 +62,7 @@ const updateReport = async (id: string, reportDto: Partial<ReportsRequest>): Pro
   return report;
 };
 
-const deleteReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
+export const deleteReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
   let report: ReportsResponse;
 
   const response = await doDelete(`${config.API_BASE_URL}reports/${id}`);
