606f9f79fedcae188fe255fcdda49446cd627b9f
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-19 23:51:58 +0500

    update Author.action.ts

--- client/src/redux/actions/Author.action.ts
+++ client/src/redux/actions/Author.action.ts
@@ -1,3 +1,15 @@
-export const ADD_AUTHOR = "ADD_AUTHOR"
-export const SET_AUTHORS = "SET_AUTHORS"
-export const REMOVE_AUTHOR = "REMOVE_AUTHOR"
+import { AuthorsResponse } from "../../types/dtos";
+
+export type AuthorActionsType = 'GET_AUTHORS' | 'SET_AUTHORS' | 'CLEAR_AUTHORS';
+
+export function typedAction(type: AuthorActionsType, payload?: any) {
+  return { type, payload };
+}
+
+export const getAuthors = () => typedAction('GET_AUTHORS');
+
+export const setAuthors = (authors: AuthorsResponse) => typedAction('SET_AUTHORS', authors);
+
+export const clearAuthors = () => typedAction('CLEAR_AUTHORS');
+
+export type AuthorAction = ReturnType<typeof getAuthors | typeof setAuthors | typeof clearAuthors>;
