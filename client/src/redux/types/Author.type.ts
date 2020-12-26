92157309b6713543daca996126cd1e1e3de033b6
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-26 14:47:30 +0500

    update Author redux types

--- client/src/redux/types/Author.type.ts
+++ client/src/redux/types/Author.type.ts
@@ -1,6 +1,24 @@
-import { AuthorsRequest } from "../../types/dtos";
+import { AuthorsResponse } from '../../types/dtos';
+
+export const GET_AUTHORS = 'GET_AUTHORS';
+export const SET_AUTHORS = 'SET_AUTHORS';
+export const CLEAR_AUTHORS = 'CLEAR_AUTHORS';
 
 export type AuthorState = {
-  authors: AuthorsRequest[]
+  authors: AuthorsResponse[]
 };
 
+interface GetAuthorsAction {
+  type: typeof GET_AUTHORS
+}
+
+interface SetAuthorsAction {
+  type: typeof SET_AUTHORS
+  payload: AuthorsResponse[]
+}
+
+interface ClearAuthorsAction {
+  type: typeof CLEAR_AUTHORS
+}
+
+export type AuthorActionType = GetAuthorsAction | SetAuthorsAction | ClearAuthorsAction;
