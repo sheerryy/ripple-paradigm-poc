c0c090abe6961d2230766b57e28301413b9ef633
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-26 14:48:51 +0500

    update Author reducer

--- client/src/redux/reducers/Author.reducer.ts
+++ client/src/redux/reducers/Author.reducer.ts
@@ -1,5 +1,4 @@
-import { AuthorState } from "../types/Author.type";
-import { AuthorAction } from "../actions/Author.action";
+import {AuthorActionType, AuthorState, CLEAR_AUTHORS, GET_AUTHORS, SET_AUTHORS,} from '../types/Author.type';
 
 const initialState: AuthorState = {
   authors: []
@@ -7,15 +6,15 @@ const initialState: AuthorState = {
 
 export function authorReducer(
   state = initialState,
-  action: AuthorAction
+  action: AuthorActionType
 ): AuthorState {
   switch (action.type) {
-    case 'GET_AUTHORS':
-      return { authors: state.authors };
-    case 'SET_AUTHORS':
-      return { authors: action.payload };
-    case 'CLEAR_AUTHORS':
-      return { authors: [] };
+    case GET_AUTHORS:
+      return { ...state, authors: state.authors };
+    case SET_AUTHORS:
+      return { ...state, authors: action.payload };
+    case CLEAR_AUTHORS:
+      return { ...state, authors: [] };
     default:
       return state;
   }
