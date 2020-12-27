e7c52b236cf19c9da35207ac767c26b3499a4cb0
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-27 14:56:22 +0500

    add Report.type.ts

--- client/src/redux/types/Report.type.ts
+++ client/src/redux/types/Report.type.ts
@@ -1,6 +1,25 @@
-import { ReportsResponse } from "../../types/dtos";
+import { ReportsResponse } from '../../types/dtos';
 
-type ReportsState = {
-  authors: ReportsResponse[]
+export const GET_REPORTS = 'GET_REPORTS';
+export const SET_REPORTS = 'SET_REPORTS';
+export const CLEAR_REPORTS = 'CLEAR_REPORTS';
+
+export type ReportsState = {
+  reports: ReportsResponse[]
 };
+
+interface GetReportsAction {
+  type: typeof GET_REPORTS;
+}
+
+interface SetReportsAction {
+  type: typeof SET_REPORTS;
+  payload: ReportsResponse[];
+}
+
+interface ClearReportsAction {
+  type: typeof CLEAR_REPORTS;
+}
+
+export type ReportActionType = GetReportsAction | SetReportsAction | ClearReportsAction;
 
