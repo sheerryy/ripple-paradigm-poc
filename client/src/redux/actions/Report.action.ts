22a821587eb68c1a7f2b3b61d798d8fefb3d4b69
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-27 14:56:37 +0500

    add Report.action.ts

--- client/src/redux/actions/Report.action.ts
+++ client/src/redux/actions/Report.action.ts
@@ -1,3 +1,28 @@
-export const ADD_REPORT = "ADD_REPORT"
-export const SET_REPORTS = "SET_REPORTS"
-export const REMOVE_REPORT = "REMOVE_REPORT"
+import {
+  GET_REPORTS,
+  SET_REPORTS,
+  CLEAR_REPORTS,
+  ReportActionType,
+} from '../types/Report.type';
+import { ReportsResponse } from '../../types/dtos';
+
+export const getReports = (): ReportActionType => {
+  return {
+    type: GET_REPORTS,
+  };
+};
+
+export const setReports = (reports: ReportsResponse[]): ReportActionType => {
+  return {
+    type: SET_REPORTS,
+    payload: reports,
+  };
+};
+
+export const clearReports = (): ReportActionType => {
+  return {
+    type: CLEAR_REPORTS,
+  };
+};
+
+export type ReportActions = ReturnType<typeof getReports | typeof setReports | typeof clearReports>;
