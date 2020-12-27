908c46e79248e302fafff465cf36665e1612e6a0
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-12-27 15:01:28 +0500

    add Report.reducer.ts

---
+++ client/src/redux/reducers/Report.reducer.ts
@@ -0,0 +1,21 @@
+import {CLEAR_REPORTS, GET_REPORTS, ReportActionType, ReportState, SET_REPORTS,} from '../types/Report.type';
+
+const initialState: ReportState = {
+  reports: []
+}
+
+export function reportReducer(
+  state = initialState,
+  action: ReportActionType
+): ReportState {
+  switch (action.type) {
+    case GET_REPORTS:
+      return { ...state, reports: state.reports };
+    case SET_REPORTS:
+      return { ...state, reports: action.payload };
+    case CLEAR_REPORTS:
+      return { ...state, reports: [] };
+    default:
+      return state;
+  }
+}
--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+
 import { authorReducer } from './Author.reducer';
+import { reportReducer } from './Report.reducer';
 
 export default combineReducers({
-  author: authorReducer
+  author: authorReducer,
+  report: reportReducer,
 });
