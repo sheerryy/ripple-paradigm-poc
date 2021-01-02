de067bdc77e00a67dd027ff63047882a3f4354e4
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-02 20:35:18 +0500

    update store.ts

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,4 +1,24 @@
-import { createStore } from 'redux'
-import rootReducer from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(rootReducer);
+import rootReducer from './reducers';
+import { AppState } from './types/App.type';
+
+import { initialAuthorState } from './reducers/Author.reducer';
+import { initialReportState } from './reducers/Report.reducer';
+
+const init: AppState = {
+  author: initialAuthorState,
+  report: initialReportState
+};
+
+export function store(initialState: AppState = init) {
+  const sagaMiddleware = createSagaMiddleware();
+  const middleware = [sagaMiddleware];
+
+  return {
+    ...createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware))),
+    runSaga: sagaMiddleware.run
+  };
+}
