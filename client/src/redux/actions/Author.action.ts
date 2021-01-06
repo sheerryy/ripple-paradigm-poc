505e09c350eabe44c61b0c73f3b107e40a363966
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-06 23:48:05 +0500

    add create author actions in Author.action.ts

--- client/src/redux/actions/Author.action.ts
+++ client/src/redux/actions/Author.action.ts
@@ -2,16 +2,13 @@ import {
   GET_AUTHORS,
   SET_AUTHORS,
   CLEAR_AUTHORS,
+  CREATE_AUTHOR_FAIL,
+  CREATE_AUTHOR_SUCCESS,
   GET_AUTHORS_ASYNC,
+  CREATE_AUTHOR_ASYNC,
   AuthorActionType,
 } from '../types/Author.type';
 import { AuthorsResponse } from '../../types/dtos';
-
-export const getAuthorsAsync = (): AuthorActionType => {
-  return {
-    type: GET_AUTHORS_ASYNC,
-  };
-};
 
 export const getAuthors = (): AuthorActionType => {
   return {
@@ -32,4 +29,36 @@ export const clearAuthors = (): AuthorActionType => {
   };
 };
 
-export type AuthorActions = ReturnType<typeof getAuthors | typeof setAuthors | typeof clearAuthors | typeof getAuthorsAsync>;
+export const createAuthorSuccess = (): AuthorActionType => {
+  return {
+    type: CREATE_AUTHOR_SUCCESS,
+  };
+};
+
+export const createAuthorFail = (): AuthorActionType => {
+  return {
+    type: CREATE_AUTHOR_FAIL,
+  };
+};
+
+export const getAuthorsAsync = (): AuthorActionType => {
+  return {
+    type: GET_AUTHORS_ASYNC,
+  };
+};
+
+export const createAuthorAsync = (): AuthorActionType => {
+  return {
+    type: CREATE_AUTHOR_ASYNC,
+  };
+};
+
+export type AuthorActions = ReturnType<
+  typeof getAuthors
+  | typeof setAuthors
+  | typeof clearAuthors
+  | typeof createAuthorFail
+  | typeof createAuthorSuccess
+  | typeof getAuthorsAsync
+  | typeof createAuthorAsync
+>;
