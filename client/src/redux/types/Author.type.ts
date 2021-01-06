8e37dce2e0778859e4047386e1f656979a71969d
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-06 23:44:52 +0500

    add create author actions types in Author.type.ts

--- client/src/redux/types/Author.type.ts
+++ client/src/redux/types/Author.type.ts
@@ -3,10 +3,15 @@ import { AuthorsResponse } from '../../types/dtos';
 export const GET_AUTHORS = 'GET_AUTHORS';
 export const SET_AUTHORS = 'SET_AUTHORS';
 export const CLEAR_AUTHORS = 'CLEAR_AUTHORS';
+export const CREATE_AUTHOR_SUCCESS = 'CREATE_AUTHOR_SUCCESS';
+export const CREATE_AUTHOR_FAIL = 'CREATE_AUTHOR_FAIL';
 export const GET_AUTHORS_ASYNC = 'GET_AUTHORS_ASYNC';
+export const CREATE_AUTHOR_ASYNC = 'CREATE_AUTHOR_ASYNC';
+
 
 export type AuthorState = {
-  authors: AuthorsResponse[]
+  authors: AuthorsResponse[],
+  newAuthor: AuthorsResponse | null,
 };
 
 interface GetAuthorsAction {
@@ -22,8 +27,26 @@ interface ClearAuthorsAction {
   type: typeof CLEAR_AUTHORS;
 }
 
+interface CreateAuthorSuccessAction {
+  type: typeof CREATE_AUTHOR_SUCCESS;
+}
+
+interface CreateAuthorFailAction {
+  type: typeof CREATE_AUTHOR_FAIL;
+}
+
 interface GetAuthorsAsyncAction {
   type: typeof GET_AUTHORS_ASYNC;
 }
 
-export type AuthorActionType = GetAuthorsAction | SetAuthorsAction | ClearAuthorsAction | GetAuthorsAsyncAction;
+interface CreateAuthorAsyncAction {
+  type: typeof CREATE_AUTHOR_ASYNC;
+}
+
+export type AuthorActionType = GetAuthorsAction
+  | SetAuthorsAction
+  | ClearAuthorsAction
+  | CreateAuthorSuccessAction
+  | CreateAuthorFailAction
+  | GetAuthorsAsyncAction
+  | CreateAuthorAsyncAction;
