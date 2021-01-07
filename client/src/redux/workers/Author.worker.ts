5831f192f66583f8d3563d32010756ba18f9dc99
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-07 23:10:57 +0500

    add createAuthorWorker in Author.worker.ts

--- client/src/redux/workers/Author.worker.ts
+++ client/src/redux/workers/Author.worker.ts
@@ -1,8 +1,20 @@
 import { call, put } from 'redux-saga/effects';
 
-import { AuthorsResponse } from "../../types/dtos";
-import { getAuthors as getAuthorsApi } from '../../apis/authors/authors.api';
-import { setAuthors as setAuthorsAction, clearAuthors as clearAuthorsAction } from "../actions/Author.action";
+import {
+  AuthorsRequest,
+  AuthorsResponse,
+} from '../../types/dtos';
+import {
+  setAuthors as setAuthorsAction,
+  clearAuthors as clearAuthorsAction,
+  createAuthorFail as createAuthorFailAction,
+  createAuthorSuccess as createAuthorSuccessAction,
+} from '../actions/Author.action';
+import {
+  getAuthors as getAuthorsApi,
+  createAuthor as createAuthorApi,
+} from '../../apis/authors/authors.api';
+import {ErrorResponse} from "../../types/responses";
 
 export function* getAllAuthorWorker() {
   try {
@@ -15,5 +27,19 @@ export function* getAllAuthorWorker() {
     yield put(setAuthorsAction(result as AuthorsResponse[]))
   } catch (err) {
     yield put(clearAuthorsAction())
+  }
+}
+
+export function* createAuthorWorker(authorDto: AuthorsRequest) {
+  try {
+    const result = yield call(createAuthorApi, authorDto);
+
+    if (result.errorCode) {
+      throw (result)
+    }
+
+    yield put(createAuthorSuccessAction(result as AuthorsResponse))
+  } catch (err) {
+    yield put(createAuthorFailAction(err as ErrorResponse))
   }
 }
