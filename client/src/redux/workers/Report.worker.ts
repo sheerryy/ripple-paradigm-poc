e51f725ee96b07fdad0cfd8f4a2308b79c2f5707
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-14 23:48:32 +0500

    update Report.worker.ts

--- client/src/redux/workers/Report.worker.ts
+++ client/src/redux/workers/Report.worker.ts
@@ -4,23 +4,24 @@ import {
   ReportsResponse,
 } from '../../types/dtos';
 import {
-  setReports as setReportsAction,
-  clearReports as clearReportsAction,
+  getReportsSuccess as getReportsSuccessAction,
+  getReportsFail as getReportsFailAction,
 } from '../actions/Report.action';
 import {
   getReports as getReportsApi,
 } from '../../apis/reports/reports.api';
+import { ErrorResponse } from '../../types/responses';
 
 export function* getAllReportWorker() {
   try {
     const result = yield call(getReportsApi);
 
     if (result.errorCode) {
-      throw new Error(result.message)
+      throw (result.message)
     }
 
-    yield put(setReportsAction(result as ReportsResponse[]))
+    yield put(getReportsSuccessAction(result as ReportsResponse[]))
   } catch (err) {
-    yield put(clearReportsAction())
+    yield put(getReportsFailAction(err as ErrorResponse))
   }
 }
