743eff764281356804b5dd3e35c5331aa44460a2
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-19 23:47:36 +0500

    fix eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import { Server } from 'http';
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
 import 'module-alias/register';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 
 import { SocketIoMiddleware } from '@middlewares';
 
-import { ReportsRoute, AuthorsRoute } from "./routes";
+import { ReportsRoute, AuthorsRoute } from './routes';
 
 const app: express.Application = express();
 const http = new Server(app);
@@ -20,15 +20,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.json({
-        app: 'ripple-server',
-        version: VERSION,
-    });
+  res.json({
+    app: 'ripple-server',
+    version: VERSION,
+  });
 });
 
 app.use('/authors', socketMiddleWare.emitterMiddleware('authors'), AuthorsRoute);
 app.use('/reports', socketMiddleWare.emitterMiddleware('reports'), ReportsRoute);
 
 const server = http.listen(PORT, () => {
-    console.log(`App started on http://localhost:${PORT}`);
+  console.log(`App started on http://localhost:${PORT}`);
 });
