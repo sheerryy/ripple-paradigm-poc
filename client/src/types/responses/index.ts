cdd247f5e9f75d0a65a06b4ecd8f5f32a2a722c9
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-23 23:05:01 +0500

    rename axios-reponse.type.ts to axios-response.type.ts

--- client/src/types/responses/index.ts
+++ client/src/types/responses/index.ts
@@ -1,3 +1,3 @@
 // Response Types
 export type ErrorResponse = import('./error-response.type').ErrorResponse;
-export type AxiosUtilResponse = import('./axios-reponse.type').AxiosUtilResponse;
+export type AxiosUtilResponse = import('./axios-response.type').AxiosUtilResponse;
