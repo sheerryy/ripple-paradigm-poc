0f735816a66787023845e897ad84d71a520c0d02
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-01-30 20:15:49 +0500

    add socket.io implementation in Author.View.tsx

--- client/src/views/author-views/Author.View.tsx
+++ client/src/views/author-views/Author.View.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import {Grid, Paper} from "@material-ui/core";
-import AuthorTable from "./components/AuthorTable";
+import React, { useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+import { Grid, Paper } from '@material-ui/core';
+
+import { getConfig } from '../../config';
+import AuthorTable from './components/AuthorTable';
+
+const { SOCKET_ENDPOINT } = getConfig();
 
 function AuthorView() {
+
+  useEffect(() => {
+    const setListeners = () => {
+      console.log('useEffect()')
+      const socketClient = socketIOClient(SOCKET_ENDPOINT);
+      socketClient.on('connect', () => console.log('connected'));
+      socketClient.on('context/authors', function(msg: string){
+        console.log('authors', msg);
+      });
+    }
+
+    setListeners()
+  }, []);
+
   return (
     <Grid container justify="center">
       <Grid item sm={12} md={10}>
         <Paper style={{padding: '2em', height: '100%'}}>
-          <AuthorTable></AuthorTable>
+          <AuthorTable />
         </Paper>
       </Grid>
     </Grid>
