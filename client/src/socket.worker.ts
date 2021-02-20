698de5f08e8c133b385119f7a3d93c0c6eae67f9
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-02-20 19:01:36 +0500

    add boilerplate of socket.worker.ts

--- client/src/socket.worker.ts
+++ client/src/socket.worker.ts
@@ -1,3 +1,19 @@
-export function socketWorker() {
+/* eslint-disable no-restricted-globals */
+export default () => {
+  self.addEventListener("message", e => {
+    if (!e) return;
 
+    console.log('Start our long running job...');
+    const seconds = 5;
+    const start = new Date().getTime();
+    const delay = seconds * 1000;
+
+    while (true) {
+      if ((new Date().getTime() - start) > delay) {
+        break;
+      }
+    }
+
+    self.postMessage("done");
+  });
 }
