764f4c6f196e1e02f52bf4b0b9439f7d01f90b14
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-02-28 21:29:17 +0500

    update socket.worker.ts

--- client/src/socket.worker.ts
+++ client/src/socket.worker.ts
@@ -1,8 +1,15 @@
 /* eslint-disable no-restricted-globals */
+
+
 export default () => {
   self.addEventListener("message", e => {
     if (!e) return;
+    const socketIOClient = require("socket.io-client");
+    const socketClient = socketIOClient('http://127.0.0.1:3001');
 
+    socketClient.on('context/emitter', function(msg: string){
+      console.log(msg);
+    });
     console.log('Start our long running job...');
     const seconds = 5;
     const start = new Date().getTime();
