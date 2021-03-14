38d6354aba919a0bd1d7243832d848124fca289f
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-03-14 23:51:23 +0500

    WIP socket.worker.ts

--- client/src/socket.worker.ts
+++ client/src/socket.worker.ts
@@ -1,26 +1,37 @@
 /* eslint-disable no-restricted-globals */
+const SocketIoWorker = () => {
+  debugger
+    importScripts(self.name + 'socket.io/socket.io.js')
+    const socketClient = io('http://127.0.0.1:3001');
 
+    const onMessage = (e: any) => {
+      if (!e) return;
 
-export default () => {
-  self.addEventListener("message", e => {
-    if (!e) return;
-    const socketIOClient = require("socket.io-client");
-    const socketClient = socketIOClient('http://127.0.0.1:3001');
+      socketClient.on('context/emitter', function (msg: string) {
+        console.log(msg);
+      });
+      console.log('Start our long running job...');
+      const seconds = 5;
+      const start = new Date().getTime();
+      const delay = seconds * 1000;
 
-    socketClient.on('context/emitter', function(msg: string){
-      console.log(msg);
-    });
-    console.log('Start our long running job...');
-    const seconds = 5;
-    const start = new Date().getTime();
-    const delay = seconds * 1000;
-
-    while (true) {
-      if ((new Date().getTime() - start) > delay) {
-        break;
+      while (true) {
+        if ((new Date().getTime() - start) > delay) {
+          break;
+        }
       }
-    }
 
-    self.postMessage("done");
-  });
+      self.postMessage("done");
+    }
+    addEventListener('message', onMessage);
 }
+
+// if (typeof window === 'object') {
+//   window.SocketIoWorker = SocketIoWorker
+// }
+export default SocketIoWorker
+// if (typeof module === 'object') {
+//   export default SocketIoWorker
+// } else {
+//   SocketIoWorker()
+// }
