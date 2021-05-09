8819eeda4642b2556d1234663564e6ed97157851
Shehryar <14bscssshoukat@seecs.edu.pk>
2021-05-09 18:06:57 +0500

    fix eslint errors in api

--- client/src/apis/authors/authors.api.ts
+++ client/src/apis/authors/authors.api.ts
@@ -7,71 +7,61 @@ import { AuthorsRequest, AuthorsResponse } from "../../types/dtos";
 const config = getConfig();
 
 export const getAuthors =  async (): Promise<AuthorsResponse[] | ErrorResponse> => {
-    let authors: AuthorsResponse[];
-
     const response =  await doGet(`${config.API_BASE_URL}authors/`);
 
     if (response.error) {
       return response.errorResponse as ErrorResponse;
     }
 
-    authors = response.data;
+    const authors = response.data as AuthorsResponse[];
 
     return authors;
 };
 
 export const getAuthor =  async (id: string): Promise<AuthorsResponse | ErrorResponse> => {
-    let author: AuthorsResponse;
-
     const response =  await doGet( `${config.API_BASE_URL}authors/${id}`);
 
     if (response.error) {
         return response.errorResponse as ErrorResponse;
     }
 
-    author = { ...response.data };
+    const author = { ...response.data } as AuthorsResponse;
 
     return author;
 };
 
 export const createAuthor =  async (authorDto: AuthorsRequest): Promise<AuthorsResponse | ErrorResponse> => {
-    let author: AuthorsResponse;
-
     const response =  await doPost( `${config.API_BASE_URL}authors/`, authorDto);
 
     if (response.error) {
         return response.errorResponse as ErrorResponse;
     }
 
-    author = { ...response.data };
+  const author = { ...response.data } as AuthorsResponse;
 
     return author;
 };
 
 export const updateAuthor =  async (id: string, authorDto: Partial<AuthorsRequest>): Promise<AuthorsResponse | ErrorResponse> => {
-  let author: AuthorsResponse;
-
   const response =  await doPut( `${config.API_BASE_URL}authors/${id}`, authorDto);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  author = { ...response.data };
+  const author = { ...response.data } as AuthorsResponse;
 
   return author;
 };
 
 export const deleteAuthor =  async (id: string): Promise<AuthorsResponse | ErrorResponse> => {
-  let author: AuthorsResponse;
-
   const response =  await doDelete( `${config.API_BASE_URL}authors/${id}`);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  author = { ...response.data };
+  const author = { ...response.data } as AuthorsResponse;
 
   return author;
 };
--- client/src/apis/reports/reports.api.ts
+++ client/src/apis/reports/reports.api.ts
@@ -7,71 +7,61 @@ import { ReportsRequest, ReportsResponse } from "../../types/dtos";
 const config = getConfig();
 
 export const getReports = async (): Promise<ReportsResponse[] | ErrorResponse> => {
-  let reports: ReportsResponse[];
-
   const response = await doGet(`${config.API_BASE_URL}reports/`);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  reports = [...response.data];
+  const reports = [...response.data] as ReportsResponse[];
 
   return reports;
 };
 
 export const getReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
-  let report: ReportsResponse;
-
   const response = await doGet(`${config.API_BASE_URL}reports/${id}`);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  report = {...response.data};
+  const report = {...response.data} as ReportsResponse;
 
   return report;
 };
 
 export const createReport = async (reportDto: ReportsRequest): Promise<ReportsResponse| ErrorResponse> => {
-  let report: ReportsResponse;
-
   const response = await doPost(`${config.API_BASE_URL}reports/`, reportDto);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  report = {...response.data};
+  const report = {...response.data} as ReportsResponse;
 
   return report;
 };
 
 export const updateReport = async (id: string, reportDto: Partial<ReportsRequest>): Promise<ReportsResponse | ErrorResponse> => {
-  let report: ReportsResponse;
-
   const response = await doPut(`${config.API_BASE_URL}reports/${id}`, reportDto);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  report = {...response.data};
+  const report = {...response.data} as ReportsResponse;
 
   return report;
 };
 
 export const deleteReport = async (id: string): Promise<ReportsResponse | ErrorResponse> => {
-  let report: ReportsResponse;
-
   const response = await doDelete(`${config.API_BASE_URL}reports/${id}`);
 
   if (response.error) {
     return response.errorResponse as ErrorResponse;
   }
 
-  report = {...response.data};
+  const report = {...response.data} as ReportsResponse;
 
   return report;
 };
